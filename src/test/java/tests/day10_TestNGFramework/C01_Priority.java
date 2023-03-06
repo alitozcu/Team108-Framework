796728a4edde5f3b0d258674ac5d6529b426dc5c
alitozcu <ali.tozcu@gmail.com>
2023-03-06 10:58:43 +0300

    il versiyon

---
+++ src/test/java/tests/day10_TestNGFramework/C01_Priority.java
@@ -0,0 +1,21 @@
+package tests.day10_TestNGFramework;
+
+import org.testng.annotations.Test;
+
+public class C01_Priority {
+
+    @Test
+    public void amazontesti(){
+        //Amazon anasayfaya gidip, amazona gittiğimizi test edelim
+    }
+
+    @Test
+    public void wisetesti(){
+        //wisequarter anasayfaya gidip, wisequartera gittiğimizi test edelim
+    }
+
+    @Test
+    public void youtubetesti(){
+        //youtube anasayfaya gidip, youtubea gittiğimizi test edelim
+    }
+}
